dd767c22399b258a3b184de70a960714954b23c5
joaomarcos11 <joaomarcos11.tech@gmail.com>
2020-07-10 15:14:16 -0300

    reconfigurando link cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://jm-m4-grades-api.herokuapp.com',
+    origin: 'https://jm-m4-grades-app.herokuapp.com',
   })
 );
 
